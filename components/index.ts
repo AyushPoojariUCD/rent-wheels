44da2ec415e693b618223615a3538fcae215149f
AyushPoojariUCD <ayush.poojari@ucdconnect.ie>
2026-02-24 22:30:38 +0530

    feat: car catalogue search bar and filter updated

--- components/index.ts
+++ components/index.ts
@@ -2,9 +2,9 @@ import Hero from './Hero';
 import CustomButton from './CustomButton';
 import Navbar from './Navbar';
 import Footer from './Footer';
-// import SearchBar from './SearchBar';
-// import CustomFilter from './CustomFilter';
-// import SearchManufacturer from './SearchManufacturer';
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomerFilter';
+import SearchManufacturer from './SearchManufacturer';
 // import CarCard from './CarCard';
 // import CarDetails from './CarDetails';
 // import ShowMore from './ShowMore';
@@ -17,7 +17,7 @@ export {
   SearchBar,
   CustomFilter,
   SearchManufacturer,
-  CarCard,
-  CarDetails,
-  ShowMore,
+  // CarCard,
+  // CarDetails,
+  // ShowMore,
 }
